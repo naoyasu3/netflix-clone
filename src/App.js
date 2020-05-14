c5179b4831b48beb00802dbe42e5b8fed2b5c1ae
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-05-14 19:47:24 +0900

    header作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/App.css";
+import { Switch, Route } from "react-router-dom";
+
+import Main from "./pages/index";
 
 function App() {
   return (
-    <div>
-      <h1>Netflix Project</h1>
-    </div>
+    <Switch>
+      <Route path="/" component={Main} />
+    </Switch>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default Main;
